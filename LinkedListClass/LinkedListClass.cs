8198643c3416f1d7e525260844b853a9ce76a5d7
Виктор Ботвинко <rusty.vik@gmail.com>
2023-02-26 11:35:04 +0000

    Merged in feature/VVCOM-2-Create-a-LinkedList-class.-Implement-basic-functionality-Add-Search-Delete-Update (pull request #1)
    VVCOM-2 <For review>
    
    * VVCOM-2 <For review>
    
    * VVCOM-2 <Fixed some mistakes after PR>
    
    * VVCOM-2 <Imroved logic_access to Node>
    
    
    Approved-by: Egor Xavr

---
+++ LinkedListClass/LinkedListClass.cs
@@ -0,0 +1,159 @@
+﻿namespace DSLinkedList;
+
+/// <summary>
+/// Describes LinkedList data structure and implements CRUD functionality.
+/// </summary>
+public class LinkedList<T>
+{
+    private Node<T>? head;
+    private Node<T>? tail;
+
+    /// <summary>
+    /// Allow to access to node through []:
+    /// 1. var q = "name of LinkedList"[index];
+    /// 2. "name of LinkedList"[index] = value;
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public T? this[int i]
+    {
+        get => Get(i);
+        set => Update(i, value);
+    }
+
+    /// <summary>
+    /// Counter of the elements in LinkedList.
+    /// </summary>
+    /// <returns></returns>
+    public int Count()
+    {
+        int count = 0;
+        
+        Node<T>? current = head;
+
+        while (current.Next != null)
+        {
+            current = current.Next;
+            count ++;
+        }
+        return count;
+    }
+
+    /// <summary>
+    /// Adding a new node in the end of the LinkedList.
+    /// If LinkedList is empty, node will be added in the first position.
+    /// </summary>
+    /// <param name="value"></param>
+    public void Add(T value)
+    {
+        Node<T>? node = new(value);
+
+        if (head is null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.Next = node;
+            tail = node;
+        }
+    }
+
+    /// <summary>
+    /// Adding a new node in a particulare place by index.
+    /// If index == 0, the node will be added in the first position.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    public void Add(int index, T value)
+    {
+        Node<T> node = new(value);
+
+        Node<T>? current = head;
+        Node<T>? previuos = null;
+        int counter = 0;
+
+        if (index == 0)
+        {
+            node.Next = head;
+            head = node;
+            if (node.Next is null)
+                tail = node;
+        }
+        else
+        {
+            while (counter != index)
+            {
+                previuos = current;
+                current = current.Next;
+
+                counter++;
+            }
+
+            previuos.Next = node;
+            node.Next = current;
+        }
+    }
+
+    /// <summary>
+    /// Delete a node by index.
+    /// </summary>
+    /// <param name="index"></param>
+    public void Delete(int index)
+    {
+        Node<T>? current = head;
+        Node<T>? previuos = null;
+        int counter = 0;
+
+        while (counter != index)
+        {
+            previuos = current;
+            current = current.Next;
+
+            counter++;
+        }
+
+        current = current.Next;
+        previuos.Next = current;
+    }
+
+    /// <summary>
+    /// Finding a value of the node by index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public T Get(int index)
+    {
+        var counter = 0;
+
+        Node<T>? node = head;
+
+        while (counter != index)
+        {
+            node = node.Next;
+            counter++;
+        }
+
+        return node.Value;
+    }
+
+    /// <summary>
+    /// Updates value of the given by index node.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    public void Update(int index, T value)
+    {
+        var counter = 0;
+
+        Node<T>? node = head;
+
+        while (counter != index)
+        {
+            node = node.Next;
+            counter++;
+        }
+        node.Value = value;
+    }
+}
---
+++ NodeClass/NodeClass.cs
@@ -0,0 +1,16 @@
+﻿namespace DSLinkedList;
+
+/// <summary>
+/// Describes an element of the data structure LinkedList.
+/// Generics are used.
+/// </summary>
+public class Node<T>
+{
+    public T? Value { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+}
